4da5434e9ee91fe05a6046309bb4541cce62f3dc
Gleb Dranitsyn <glebdranitsyn@gmail.com>
2026-04-04 12:28:57 +0300

    Добавьте файлы проекта.

---
+++ SaunakulaApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SaunakulaApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SaunakulaApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SaunakulaApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SaunakulaApp.Pages")]
