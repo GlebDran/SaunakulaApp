9f2e69397d7d30f0e6d267a5381228a5f7116178
Gleb Dranitsyn <glebdranitsyn@gmail.com>
2026-04-04 12:41:55 +0300

    add AppShell with 4 tabs, color palette and styles matching Figma design

--- SaunakulaApp/AppShell.xaml.cs
+++ SaunakulaApp/AppShell.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace SaunakulaApp
+﻿using SaunakulaApp.Views;
+
+namespace SaunakulaApp;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        Routing.RegisterRoute(nameof(HouseDetailsPage), typeof(HouseDetailsPage));
+        Routing.RegisterRoute(nameof(BookingPage), typeof(BookingPage));
+        Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
+        Routing.RegisterRoute(nameof(RegisterPage), typeof(RegisterPage));
     }
 }
