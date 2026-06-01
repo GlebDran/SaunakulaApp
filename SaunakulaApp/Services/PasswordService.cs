4605295de26a008fc6ac6b25ae13340029175dc7
Gleb Dranitsyn <glebdranitsyn@gmail.com>
2026-06-01 19:07:42 +0300

    Add password hash service

---
+++ SaunakulaApp/Services/PasswordService.cs
@@ -0,0 +1,14 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SaunakulaApp.Services;
+
+public static class PasswordService
+{
+    public static string HashPassword(string password)
+    {
+        using var sha = SHA256.Create();
+        var bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(password));
+        return Convert.ToHexString(bytes);
+    }
+}
