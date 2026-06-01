02834b7e02b199b79de993d818be384eb54522bf
Gleb Dranitsyn <glebdranitsyn@gmail.com>
2026-06-01 19:25:09 +0300

    Register register view model

--- SaunakulaApp/MauiProgram.cs
+++ SaunakulaApp/MauiProgram.cs
@@ -32,6 +32,7 @@ public static class MauiProgram
         builder.Services.AddTransient<HouseDetailsViewModel>();
         builder.Services.AddTransient<BookingViewModel>();
         builder.Services.AddTransient<LoginViewModel>();
+        builder.Services.AddTransient<RegisterViewModel>();
 
         // Views
         builder.Services.AddTransient<SplashPage>();
